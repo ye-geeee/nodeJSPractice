634cac5d58393c8f3d578bebd6443d84f4830fae
Yeji Kang <60929159+ye-geeee@users.noreply.github.com>
2020-04-29 23:43:50 +0900

    [Login] Add Login Session (#4)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  const sess = req.session;
+  res.render('index', {
+    nickname: sess.user_uid+1? users[sess.user_uid]['user_nickname'] : ''
+  });});
 
 module.exports = router;
